4b5fa6cca4f2043b0bd74c896726e0124ea60daf
Öncüm Korkmaz Yılmaz <oncumkorkmaz@gmail.com>
2021-10-29 13:53:29 +0300

    connected to websocket

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,69 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import {MainClient} from'binance';
+import {MainClient,WebsocketClient} from'binance';
 
-const client = new MainClient({
-    api_key: process.env.BNC_API,
-    api_secret: process.env.BNC_SECRET,
-    baseUrl:'https://testnet.binance.vision',
-    
-  });
+// const client = new MainClient({
+//     api_key: process.env.BNC_API,
+//     api_secret: process.env.BNC_SECRET,  
+//   });
 
-//client.requestUrl = 'https://testnet.binance.vision/api'
+// client.getAssetDetail({asset : 'BTC'})
+// .then(r=>{
+//     console.log("info anan:" , r);
+// })
+// .catch(e => {
+//     console.log("error anan :" , e);
+// })
 
-client.getAccountInformation()
-.then(r=>{
-    console.log("info anan:" , r);
+// client.getSymbolPriceTicker({symbol : 'BTCUSDT'})
+// .then(r=>{
+//     console.log("info anan:" , r);
+// })
+// .catch(e => {
+//     console.log("error anan :" , e);
+// })
+
+
+const wsClient = new WebsocketClient({
+  api_key: process.env.BNC_API,
+  api_secret: process.env.BNC_SECRET,
+  beautify: true,
 })
-.catch(e => {
-    console.log("error anan :" , e);
-})
+
+wsClient.on('message', (data) => {
+    console.log('raw message received ', JSON.stringify(data, null, 2));
+});
+
+wsClient.on('formattedMessage', (data) => {
+    if (!Array.isArray(data)) {
+      if (data.eventType === 'kline') {
+        console.log('kline received ', data.kline);
+        return;
+      }
+      if (data.eventType === '24hrTicker') {
+        console.log('24hrTicker received ', data);
+        return;
+      }
+    }
+    console.log('log formattedMessage: ', data);
+});
+
+wsClient.on('open', (data) => {
+    console.log('connection opened open:', data.wsKey, data.ws.target.url);
+});
+
+// response to command sent via WS stream (e.g LIST_SUBSCRIPTIONS)
+wsClient.on('reply', (data) => {
+    console.log('log reply: ', JSON.stringify(data, null, 2));
+});
+
+wsClient.on('reconnecting', (data) => {
+    console.log('ws automatically reconnecting.... ', data?.wsKey );
+});
+
+wsClient.on('reconnected', (data) => {
+    console.log('ws has reconnected ', data?.wsKey );
+});
+
+wsClient.subscribeKlines('BTCUSDT', '1m', 'usdm');
