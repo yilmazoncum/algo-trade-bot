cd45e8316e85e9e43b7972afebc01e2f2a04db4b
yilmazoncum <oncumkorkmaz@gmail.com>
2022-02-03 14:25:16 +0300

    add candlesticks methods

--- src/app.js
+++ src/app.js
@@ -41,6 +41,30 @@ binance.cancel("BTCUSDT", orderid, (error, response, symbol) => {
   console.info(symbol+" cancel response:", response);
 });  */
 
-setTimeout(() => {
+/* setTimeout(() => {
   console.info( binance.cancelAll("BTCUSDT") );
-},20000);
+},20000); */
+
+/* binance.websockets.chart("BNBBTC", "1m", (symbol, interval, chart) => {
+  let tick = binance.last(chart);
+  const last = chart[tick].close;
+  console.info(chart);
+  // Optionally convert 'chart' object to array:
+  // let ohlc = binance.ohlc(chart);
+  // console.info(symbol, ohlc);
+  console.info(symbol+" last price: "+last)
+}); */ //most recent 500 candle
+
+// Periods: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+/* binance.websockets.candlesticks(['BNBBTC'], "1m", (candlesticks) => {
+  let { e:eventType, E:eventTime, s:symbol, k:ticks } = candlesticks;
+  let { o:open, h:high, l:low, c:close, v:volume, n:trades, i:interval, x:isFinal, q:quoteVolume, V:buyVolume, Q:quoteBuyVolume } = ticks;
+  console.info(symbol+" "+interval+" candlestick update");
+  console.info("open: "+open);
+  console.info("high: "+high);
+  console.info("low: "+low);
+  console.info("close: "+close);
+  console.info("volume: "+volume);
+  console.info("isFinal: "+isFinal);
+}); //candle update realtime */
+
