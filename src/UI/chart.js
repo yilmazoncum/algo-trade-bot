3a909d3f46a0f10e73c0dbec0042cd330e231a21
yilmazoncum <oncumkorkmaz@gmail.com>
2022-02-13 12:09:35 +0300

    make indicators.js functional

--- src/UI/chart.js
+++ src/UI/chart.js
@@ -7,16 +7,16 @@ const chart = LightweightCharts.createChart(document.body, {
 const candlestickSeries = chart.addCandlestickSeries(); //make this chart to candlestick chart
 
 var livePriceData = [
-  { time: "2018-12-22", open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
+  /* { time: "2018-12-22", open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
   { time: "2018-12-23", open: 45.12, high: 53.9, low: 45.12, close: 48.09 },
   { time: "2018-12-24", open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
   { time: "2018-12-25", open: 68.26, high: 68.26, low: 59.04, close: 60.5 },
   { time: "2018-12-26", open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
   { time: "2018-12-27", open: 91.04, high: 121.4, low: 82.7, close: 111.4 },
-  {time: "2018-12-28", open: 111.51, high: 142.83,low: 103.34,close: 131.25,},
+  { time: "2018-12-28", open: 111.51, high: 142.83,low: 103.34,close: 131.25,},
   { time: "2018-12-29", open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
   { time: "2018-12-30", open: 106.33, high: 110.2, low: 90.39, close: 98.1 },
-  { time: "2018-12-31", open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
+  { time: "2018-12-31", open: 109.87, high: 114.69, low: 85.66, close: 111.26 }, */
 ]; //starting prices for demo
 
 candlestickSeries.setData(livePriceData); //show the prices on chart
@@ -28,7 +28,7 @@ ws.addEventListener("open", () => {
 ws.addEventListener("message", (e) => {
   var data = JSON.parse(e.data);
   let dump = {
-    time: data["eventTime: "],
+    time: parseInt(data["eventTime: "]/1000 - ((data["eventTime: "]/1000)%100)), //! its only working for 1m chart it should be fixed
     open: data["open: "],
     high: data["high: "],
     low: data["low: "],
@@ -36,5 +36,5 @@ ws.addEventListener("message", (e) => {
   };
   console.log(dump);
   livePriceData.push(dump); //update prices array with new ones
-  candlestickSeries.setData(livePriceData); // update charts with new array
+  candlestickSeries.update(dump); // update charts with new array
 });
--- src/root/app.js
+++ src/root/app.js
@@ -1,3 +1,5 @@
+import {getBB} from './indicators.js';
+
 import WebSocket from "ws";
 const wss = new WebSocket.Server({ port: 8086 });
 
@@ -56,18 +58,7 @@ wss.on("connection", (ws) => {
   // Periods: 1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
   binance.websockets.candlesticks(["BNBBTC"], "1m", (candlesticks) => {
     let { e: eventType, E: eventTime, s: symbol, k: ticks } = candlesticks;
-    let {
-      o: open,
-      h: high,
-      l: low,
-      c: close,
-      v: volume,
-      n: trades,
-      i: interval,
-      x: isFinal,
-      q: quoteVolume,
-      V: buyVolume,
-      Q: quoteBuyVolume,
+    let {o: open,h: high, l: low, c: close, v: volume, n: trades, i: interval,x: isFinal, q: quoteVolume,V: buyVolume,Q: quoteBuyVolume,
     } = ticks;
 
     console.info(symbol + " " + interval + " candlestick update");
@@ -80,8 +71,11 @@ wss.on("connection", (ws) => {
       "eventTime: ": eventTime,
     };
     console.log(obj);
+    getBB(JSON.stringify(obj));
     ws.send(JSON.stringify(obj));
   }); //candle update realtime
+
+  
 
   ws.on("message", (data) => {
     console.log("client has sent us:" + data);
@@ -91,6 +85,8 @@ wss.on("connection", (ws) => {
     console.log("client has disconnected");
   });
 });
+
+
 
 /* let quantity = 0.00046, price = 30000.00000000;
 binance.buy("BTCUSDT", quantity, price, {type:'LIMIT'}, (error, response) => {
